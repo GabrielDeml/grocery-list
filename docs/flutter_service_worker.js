0189eab1606bbda69e2889472e631f6c038d8955
GabrielDeml <gabrielddeml@gmail.com>
2022-01-15 09:55:29 -0500

    Fixed it?

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4ae7fdaa4254f090c45b8854b6af153b",
-"/": "4ae7fdaa4254f090c45b8854b6af153b",
+"index.html": "bad8948819788439b26d573b5c7b2abd",
+"/": "bad8948819788439b26d573b5c7b2abd",
 "manifest.json": "7394d18fc432793e3609a91792d4e969"
 };
 
