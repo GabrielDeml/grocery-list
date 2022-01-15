453671a2428e2bf47a7a31d211b321ef9f848aab
GabrielDeml <gabrielddeml@gmail.com>
2022-01-15 08:05:05 -0500

    Added menu bar

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'The best grocery list'),
     );
   }
 }
@@ -43,12 +43,39 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
+  
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
 
   void _incrementCounter() {
     setState(() {
@@ -75,24 +102,47 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(20),
-              children: const [
-                Text("Hello World!"),
-                Text("Hello World!"),
-                Text("Hello World!"),
-                Text("Hello World!"),
-                Text("Hello World!"),
-              ],
-            ),
-          ],
-        ),
+       bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      
+      // Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       ListView(
+      //         shrinkWrap: true,
+      //         padding: const EdgeInsets.all(20),
+      //         children: const [
+      //           Text("Hello World!"),
+      //           Text("Hello World!"),
+      //           Text("Hello World!"),
+      //           Text("Hello World!"),
+      //           Text("Hello World!"),
+      //         ],
+      //       ),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
